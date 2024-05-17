aa23cfad3b7b5453f7c092e2c3d1e12bff568fb7
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-17 09:38:50 +0300

    save

--- src/modules/auth/controller/auth.contorller.ts
+++ src/modules/auth/controller/auth.contorller.ts
@@ -1,20 +1,15 @@
-import {
-  Body,
-  // ConflictException,
-  Controller,
-  Post,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, Post, UseFilters, UsePipes } from '@nestjs/common';
 
 import {
   CreateUserDto,
   createUserSchema,
 } from 'src/modules/users/dto/users.dto';
 import { ZodValidationPipe } from 'src/modules/users/pipe/users.pipe';
-
 import { AuthService } from '../service/auth.service';
+import { AuthExceptionFilter } from '../exception/auth.exception';
 
 @Controller('auth')
+@UseFilters(AuthExceptionFilter)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
@@ -22,9 +17,5 @@ export class AuthController {
   @UsePipes(new ZodValidationPipe(createUserSchema))
   async createUser(@Body() createUserDto: CreateUserDto) {
     await this.authService.signUp(createUserDto);
-    // try {
-    // } catch (error) {
-    //   throw new ConflictException(error.message);
-    // }
   }
 }
---
+++ src/modules/auth/exception/auth.exception.ts
@@ -0,0 +1,19 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { MongoError } from 'mongodb';
+import { AppService } from 'src/services';
+
+@Catch(MongoError)
+export class AuthExceptionFilter extends AppService implements ExceptionFilter {
+  constructor() {
+    super();
+  }
+
+  catch(exception: MongoError, host: ArgumentsHost) {
+    const responseMessage = this.response(host);
+
+    switch (exception.code) {
+      case 11000:
+        responseMessage(exception.name, exception.message, 400);
+    }
+  }
+}
