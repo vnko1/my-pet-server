5046794bdb63f71ead8573ce65c95798d7ccab56
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-17 09:18:45 +0300

    save

--- src/services/exceptions/app.exception.ts
+++ src/services/exceptions/app.exception.ts
@@ -6,35 +6,24 @@ import {
   InternalServerErrorException,
   HttpStatus,
 } from '@nestjs/common';
+import { AppService } from '../services/app.service';
 
 @Catch()
-export class AppHttpExceptionFilter implements ExceptionFilter {
+export class AppHttpExceptionFilter
+  extends AppService
+  implements ExceptionFilter
+{
+  constructor() {
+    super();
+  }
   catch(exception: InternalServerErrorException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-    const request = ctx.getRequest();
-
-    const responseMessage = (
-      type: string,
-      message: string,
-      status: HttpStatus,
-    ) => {
-      response.status(status).json({
-        statusCode: status,
-        path: request.url,
-        errorType: type,
-        errorMessage: message,
-      });
-    };
-
+    const responseMessage = this.response(host);
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    if (exception.message.startsWith('E11000')) {
-      responseMessage('Error', exception.message, 400);
-    } else if (exception.message) {
+    if (exception.message) {
       responseMessage('Error', exception.message, status);
     } else {
       responseMessage(exception.name, exception.message, status);
--- src/services/services/app.service.ts
+++ src/services/services/app.service.ts
@@ -1,3 +1,4 @@
+import { ArgumentsHost, HttpStatus } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 export abstract class AppService {
@@ -32,5 +33,20 @@ export abstract class AppService {
 
   protected getSkipPattern(page: string | number = this.page, limit: number) {
     return +page > 0 ? (+page - 1) * limit : 0;
+  }
+
+  protected response(host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    return function (type: string, message: string, status: HttpStatus) {
+      return response.status(status).json({
+        statusCode: status,
+        path: request.url,
+        errorType: type,
+        errorMessage: message,
+      });
+    };
   }
 }
