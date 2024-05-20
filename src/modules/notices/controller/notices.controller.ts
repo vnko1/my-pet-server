47edd9b3d66b52f8232c5272095e670eb7999ccb
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-20 10:23:12 +0300

    save

--- src/modules/notices/controller/notices.controller.ts
+++ src/modules/notices/controller/notices.controller.ts
@@ -1,13 +1,21 @@
 import {
   Controller,
+  Delete,
   Get,
+  Param,
   Post,
   Req,
+  UploadedFile,
   UseFilters,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
 import { AuthGuard, MongooseExceptionFilter } from 'src/common';
 import { IUserId } from 'src/types';
+import { multerStorageConfig } from 'src/utils';
 
 @Controller('notices')
 @UseFilters(MongooseExceptionFilter)
@@ -19,7 +27,38 @@ export class NoticesController {
     return req?.user.id;
   }
 
+  @Get(':id')
+  getNotice(@Param('id') id: string) {}
+
+  @UseGuards(AuthGuard)
+  @Post('favorites')
+  async addToFavorites(@Req() req: IUserId) {}
+
+  @UseGuards(AuthGuard)
+  @Get('favorites')
+  async getFavorites(@Req() req: IUserId) {}
+
+  @UseGuards(AuthGuard)
+  @Delete('favorites')
+  async removeFromFavorites(@Req() req: IUserId) {}
+
   @UseGuards(AuthGuard)
   @Post()
-  async addNotice() {}
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage(multerStorageConfig),
+    }),
+  )
+  async addNotice(
+    @Req() req: IUserId,
+    @UploadedFile() image: Express.Multer.File,
+  ) {}
+
+  @UseGuards(AuthGuard)
+  @Get('owner')
+  async getOwnerNotices(@Req() req: IUserId) {}
+
+  @UseGuards(AuthGuard)
+  @Delete(':id')
+  async deleteNotice(@Param('id') id: string) {}
 }
